5d00d8644110d4d880e53a979aaf8c2cbe012a5c
Frankline Were <franklinegift@gmail.com>
2024-06-29 17:59:36 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+// multiply
+let num1 = 62;
+let num2 = 1;
+let multiply =num1 * num2;
+
+//random
+let random = Math.floor(Math.random() * 100) + 1;
+
+//mod
+let num3 = 8992 
+let num4 = 42;
+let mod = num3 % num4;
+
+//max
+let max = Math.max(1, 5, 10, 20);
